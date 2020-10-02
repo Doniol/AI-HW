356c483646017da05304c9a6af9935e69edeec35
Doniol <43216456+Doniol@users.noreply.github.com>
2020-10-02 09:36:43 +0200

    Add files via upload

---
+++ Matrix NN's/backprop_gereviseerd.py
@@ -0,0 +1,135 @@
+# Gereviseerde versie van backprop.py  (zodanig dat ik (MV) hem zelf begrijp en kan uitleggen)
+
+# meer over numpy array slicing and manipulating:
+# https://machinelearningmastery.com/index-slice-reshape-numpy-arrays-machine-learning-python/
+
+import numpy as np
+from math import e
+import random
+
+random.seed(1)
+
+def sigmoid(x):
+    """Standard sigmoid; since it relies on ** to do computation, it broadcasts on vectors and matrices"""
+    return 1 / (1 + (e**(-x)))
+
+def derivative_sigmoid(x):
+    """Expects input x to be already sigmoid-ed"""
+    return x * (1 - x)
+
+def tanh(x):
+    """Standard tanh; since it relies on ** and * to do computation, it broadcasts on vectors and matrices"""
+    return (e ** (2*x) - 1) / (e ** (2*x) + 1)
+
+def derived_tanh(x):
+    """Expects input x to already be tanh-ed."""
+    return 1 - x*x
+
+# NB: veel sneller zou natuurlijk zijn om 1x forward propagation toe te passen, en vervolgens tijdens de backprop-stap
+# de resultaten daarvan op te halen, ipv ze steeds opnieuw te berekenen tot een bepaalde layer..
+# maar goed, doordat we dat cachen nu weglaten blijft de code wat eenvoudiger. Vooruit dus, voor nu..
+def forward(inputs,weights,function=sigmoid,layer=-1):
+    """Function needed to calculate activation on a particular layer 
+    (WITHOUT PREPENDING 1 as output, but with prepending 1 in its input).
+    The weights array consists of a weight matrix for each layer.
+    The first weight of each row of such a weight matrix is the bias weight.
+    layer=-1 calculates all layers, thus provides the output of the network
+    layer=0 returns the inputs
+    any layer in between, returns the output vector of that particular (hidden) layer"""
+    if layer == 0:
+        return inputs
+    elif layer == -1:
+        layer = len(weights) - 1
+    
+    function = np.vectorize(function)
+    for weight in weights:
+        inputs = function(np.dot(weight, np.insert(inputs, 0, 1)))
+    return inputs#TODO FIX DAT IE NIET ALLES DOET
+
+    
+def backprop(inputs, outputs, weights, function=sigmoid, derivative=derivative_sigmoid, eta=0.01):
+    """
+    Function to calculate deltas matrix based on gradient descent / backpropagation algorithm.
+    Deltas matrix represents the changes that are needed to be performed to the weights (per layer) to
+    improve the performance of the neural net.
+    :param inputs: (numpy) array representing the input vector of the trainingsample.
+    :param outputs:  (numpy) array representing the desired output vector from the training sample.
+    :param weights:  list of numpy arrays (matrices) that represent the weights per layer.
+    :param function: activation function to be used, e.g. sigmoid or tanh
+    :param derivative: derivative of activation function to be used.
+    :param learnrate: rate of learning.
+    :return: list of numpy arrays representing the delta per weight per layer.
+    """
+    inputs = np.array(inputs)
+    outputs = np.array(outputs)
+    updates = []
+    
+    layers = len(weights) # set current layer to output layer +1, because 0 means input layer, for which there are no weights.
+    
+    for layer in range(layers,0,-1):    # layer 0 behandelen we niet in de backpropagation, want dat zijn slechts inputs.
+        a_now = forward(inputs, weights, function, layer)    # calculate activation of a layer
+        a_prev = forward(inputs, weights, function, layer-1) # calculate activation of a layer
+        if layer == layers:
+            # it is the output layer.
+            print("HERE")
+            print(derivative(a_now), outputs, a_now)
+            delta = np.array(derivative(a_now) * (outputs - a_now)).T  # calculate error on output
+        else:
+            delta = derivative(a_now) * weights[layer][:,1:].T.dot(delta) # calculate error on current layer (that rule needs to work with the weights excluding the biases only. therefore, discard the first column with bias-weights.
+            
+        thetaUpdate = eta * np.outer(delta,np.append(1, a_prev)) # calculate adjustments to weights
+        print("NOW HERE")
+        print(weights, layer)
+        print(thetaUpdate)
+        updates.insert(0, thetaUpdate) # store adjustments
+
+    return updates
+        
+# updating weights:
+# given an array w of numpy arrays per layer and the deltas calculated by backprop, do
+# for index in range(len(w)):
+#     w[index] = w[index] + updates[index]
+
+
+def get_random_weights(neuron_count, input_count, weight_0_location=[]):
+    # weight_0_location is list filled with coordinates: [input, neuron], sorted by input low to high
+    weights = []
+    for neuron in range(0, neuron_count):
+        temp_weights = []
+        for input in range(0, input_count):
+            if weight_0_location:
+                if input == weight_0_location[0][0] and neuron == weight_0_location[0][1]:
+                    temp_weights.append(0)
+                    weight_0_location.pop(0)
+                else:
+                    temp_weights.append(random.uniform(-1, 1))
+            else:
+                temp_weights.append(random.uniform(-1, 1))
+        weights.append(temp_weights)
+    return weights
+
+
+def main():
+    training_data = [[0, 0], [1, 0], [0, 1], [1, 1]]
+    training_solutions = [0, 1, 1, 0]
+    learning_rate = 0.1
+    weights_1 = np.array(get_random_weights(2, len(training_data[0]) + 1))
+    weights_2 = np.array(get_random_weights(1, len(training_data[0]) + 1))
+    weights = [weights_1, weights_2]
+    print(weights)
+    iterations = 400
+
+    # for i in range(0, iterations):
+    for training_index in range(0, len(training_data)):
+        weight_change = backprop(training_data[training_index], training_solutions[training_index], weights, tanh, derived_tanh, learning_rate)
+        print("1", weights)
+        print("2", weight_change)
+        for weight_index in range(0, len(weights)):
+            print(3, weight_index)
+            print(weights[weight_index])
+            print(weight_change[weight_index])
+            print(weights[weight_index] + weight_change[weight_index])
+
+
+
+main()
---
+++ Matrix NN's/math_NN.py
@@ -0,0 +1,160 @@
+import numpy as np
+import math
+import mpmath
+import random
+
+random.seed(1)
+
+
+def tanh(x: float) -> float:
+    ''' This function calculates the tanh of the given input
+    
+    return: The float of the calculated tanh
+    '''
+    return (math.exp(x) - math.exp(-x)) / (math.exp(x) + math.exp(-x)) 
+
+
+def tanh_array(array):
+    output = []
+    for entry in array:
+        output.append(tanh(entry))
+    return output
+
+
+def get_random_weights(neuron_count, input_count, weight_0_location=[]):
+    # weight_0_location is list filled with coordinates: [input, neuron], sorted by input low to high
+    weights = []
+    for neuron in range(0, neuron_count):
+        temp_weights = []
+        for input in range(0, input_count):
+            if weight_0_location:
+                if input == weight_0_location[0][0] and neuron == weight_0_location[0][1]:
+                    temp_weights.append(0)
+                    weight_0_location.pop(0)
+                else:
+                    temp_weights.append(random.uniform(-1, 1))
+            else:
+                temp_weights.append(random.uniform(-1, 1))
+        weights.append(temp_weights)
+    return weights
+            
+
+def get_random_inputs(input_count):
+    inputs = []
+    for input in range(0, input_count):
+        inputs.append(random.uniform(-1, 1))
+    return inputs
+
+
+def calc_a(OG_inputs, weights_all, function):
+    inputs = OG_inputs
+    for weights in weights_all:
+        inputs = function(np.dot(weights, np.insert(inputs, 0, 1)))
+    return inputs
+
+
+def calc_C(results, training_solutions):
+    return sum([x**2 for x in np.array(results) - np.array(training_solutions)])
+
+
+def calc_parallel_a(OG_matrix_inputs, weights_all, function):
+    matrix_inputs = OG_matrix_inputs
+    for weights in weights_all:
+        matrix_inputs = np.dot(weights, matrix_inputs)
+        tanh_inputs = []
+        for array in matrix_inputs:
+            tanh_inputs.append(function(array))
+        matrix_inputs = np.ones([matrix_inputs.shape[0] + 1, matrix_inputs.shape[1]])
+        matrix_inputs[1:, :] = tanh_inputs
+    return matrix_inputs[1:, :]
+
+
+def calc_parallel_a_test(OG_matrix_inputs, weights_all, function):
+    matrix_inputs = OG_matrix_inputs
+    for weights in weights_all:
+        new_inputs = function(np.dot(weights, matrix_inputs))
+        matrix_inputs = np.ones([new_inputs.shape[0] + 1, new_inputs.shape[1]])
+        matrix_inputs[1:, :] = new_inputs
+    return matrix_inputs[1:, :]
+
+
+def main():
+    tanh_vectorized = np.vectorize(math.tanh)
+    # Every node needs a weight for every node/input in previous layer, if said node/input is not connected to all nodes then its weight equals 0
+    # Test part 1: final output
+    print("Test 1")
+    # Test No1
+    print("No1")
+    inputs_1 = [0, 1]
+    bias = [1]
+    actual_inputs_1 = np.array(np.insert(inputs_1, 0, bias))
+    weights_1 = np.array(get_random_weights(2, len(actual_inputs_1)))
+    outputs_1 = np.dot(weights_1, actual_inputs_1)
+    results_1 = tanh_vectorized(outputs_1)
+
+    inputs_2 = results_1
+    actual_inputs_2 = np.array(np.insert(inputs_2, 0, bias))
+    weights_2 = np.array(get_random_weights(1, len(actual_inputs_2)))
+    outputs_2 = np.dot(weights_2, actual_inputs_2)
+    results_2 = tanh_vectorized(outputs_2)
+    print(results_2)
+
+    # Test No2
+    print("No2")
+    results_1_1 = tanh_array(outputs_1)
+    inputs_2_1 = results_1_1
+    actual_inputs_2_1 = np.array(np.insert(inputs_2_1, 0, bias))
+    outputs_2_1 = np.dot(weights_2, actual_inputs_2_1)
+    results_2_1 = tanh_array(outputs_2_1)
+    print(results_2_1)
+
+    # Test No3
+    print("No3")
+    print(inputs_1)
+    print(calc_a(inputs_1, [weights_1, weights_2], tanh_vectorized)[0])
+
+    # Test No4
+    print("No4")
+    new_inputs = np.array([[0], [1]])
+    print(new_inputs)
+    print(calc_a(new_inputs, [weights_1, weights_2], tanh_vectorized)[0])
+    
+    # Test part 2: Cost
+    print("Test 2")
+    training_data = [[0, 0], [1, 0], [0, 1], [1, 1]]
+    training_solutions = [0, 1, 1, 0]
+    results = []
+    for training in training_data:
+        results.append(calc_a(training, [weights_1, weights_2], tanh_vectorized)[0])
+    print(results)
+    
+    # Test No1
+    print("No1")
+    cost = 0
+    for result in range(0, len(results)):
+        cost += (results[result] - training_solutions[result])**2
+    print(cost)
+
+    # Test No2
+    print("No2")
+    print(calc_C(results, training_solutions))
+
+    # Test part 3: further parallelisation
+    print("Test 3")
+    # Test No1
+    print("No1")
+    for training in training_data:
+        print(calc_a(training, [weights_1, weights_2], tanh_vectorized))
+
+    # Test No2
+    print("No2")
+    inputs_parallel = np.array([[1, 1, 1, 1], [0, 1, 0, 1], [0, 0, 1, 1]])
+    print(calc_parallel_a(inputs_parallel, [weights_1, weights_2], tanh_vectorized))
+
+    # Test No3
+    print("No3")
+    tanh_vectorized_twice = np.vectorize(tanh_vectorized)
+    print(calc_parallel_a_test(inputs_parallel, [weights_1, weights_2], tanh_vectorized_twice))
+
+
+main()
